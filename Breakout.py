083c79f567ac52c9182a937a5fb53a168eb3aa7d
Ethan Tron <tranet@mail.gvsu.edu>
2022-04-04 11:27:58 -0400

    Update

--- Breakout.py
+++ Breakout.py
@@ -9,9 +9,36 @@ BRICK_WID = 10
 BRICK_LEN = 5
 PADDLE_WID = 200
 PADDLE_HIG = 15
+SCORE_WID = 200
+SCORE_HIG = 50
 
 BALL_SIZE = 10
 
+# class Text(pg.sprite.Sprite):
+#     def __init__(self, text, size, color, width, height):
+#         # Call the parent class (Sprite) constructor  
+#         pg.sprite.Sprite.__init__(self)
+    
+#         self.font = pg.font.SysFont("Arial", size)
+#         self.textSurf = self.font.render(text, 1, color)
+#         self.image = pg.Surface((width, height))
+#         W = self.textSurf.get_width()
+#         H = self.textSurf.get_height()
+#         self.image.blit(self.textSurf, [width/2 - W/2, height/2 - H/2])
+
+# class Score(pg.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = pg.Surface((SCORE_WID, SCORE_HIG))
+#         self.font = pg.font.Font(pg.font.get_default_font(), 32)
+#         self.text = self.font.render('0', False, (5,255,5), (0,0,255))
+#         self.rect = self.image.get_rect()
+#         self.rect.x = X_RES - SCORE_WID
+#         self.rect.y = Y_RES - SCORE_HIG
+#         self.rect.center = (X_RES // 2, Y_RES // 2)
+
+#     def update(self):
+#         pass
 
 class Paddle(pg.sprite.Sprite):
     def __init__(self):
@@ -29,19 +56,23 @@ class Paddle(pg.sprite.Sprite):
         if self.rect.x + PADDLE_WID < X_RES:
             self.rect.x += 5
 
+
 class Ball(pg.sprite.Sprite):
     bricks = None
     paddle = None
     def __init__(self):
         super().__init__()
-        # r = random.randint
+        r = random.randint
         self.image = pg.Surface((BALL_SIZE, BALL_SIZE))
         self.rect = self.image.get_rect()
         pg.draw.circle(self.image, (0, 101, 164), (32, 32), 32)
         self.rect.x = X_RES // 2
         self.rect.y = Y_RES // 2
-        self.velocity = [3, 3]
+        self.velocity = [r(-3,3), r(2,4)]
         self.dead = False
+        self.boom = pg.mixer.Sound("boom.mp3")
+        # self.pew = pg.mixer.Sound('./pew.wav')
+        # self.pew.play()
 
     def update(self):
         collisions = pg.sprite.spritecollide(self, Ball.bricks, False)
@@ -99,7 +130,6 @@ class Brick(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        # self.boom = pg.mixer.Sound("./boom.mp3")
 
 
 class Game:
@@ -111,23 +141,33 @@ class Game:
         self.bricks = pg.sprite.Group()
         self.balls = pg.sprite.Group()
         self.paddle = pg.sprite.Group()
+        self.score = pg.sprite.Group()
+        self.bg = pg.image.load("bg.jpg")
+        self.lives = 3
+
 
     def run(self):
+        myfont = pg.font.SysFont('Comic Sans MS', 30)
+        textsurface = myfont.render('Some Text', False, (0, 0, 0))
         while self.__running:
-            if (self.isGamerOver()):
-                pg.display.set_caption('Show Text')
-                font = pg.font.Font('freesansbold.ttf', 32)
-                text = font.render('YOU LOST!', True, (255, 0, 0), (0,0,0))
-                textRect = text.get_rect()
-                textRect.center = (X_RES // 2, Y_RES // 8 * 7)
-                print("Game over!!!")
+            if (self.isAlive()):
+                if self.lives > 0:
+                    self.balls.add(Ball())
+                    self.lives -= 1
+                else:
+                    print("Game over!!!")
             events = pg.event.get()
             for event in events:
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_r:
+                        self.balls.add(Ball())
                 if event.type == pg.QUIT:
                     self.__running = False
                     pg.quit()
                     exit()
             key = pg.key.get_pressed()
+            # if key[pg.K_r]:
+            #     self.balls.add(Ball())
             if key[pg.K_LEFT]:
                 for paddle in self.paddle:
                     paddle.moveLeft()
@@ -137,21 +177,22 @@ class Game:
             # Take events
 
             # Update updateable objects
-            # self.bricks.update()
             self.balls.update()
-            self.paddle.update()
             for ball in self.balls:
                 if ball.dead == True:
                     self.balls.remove_internal(ball)
-            # Redraw
-            self.screen.fill( (255, 255, 255) )
+            # Redraw#
+            # INSIDE OF THE GAME LOOP
+            self.screen.blit(textsurface,(0,0))
+            self.screen.blit(self.bg, (0, 0))
             self.bricks.draw(self.screen)
             self.balls.draw(self.screen)
             self.paddle.draw(self.screen)
+            self.score.draw(self.screen)
             pg.display.flip()
             self.clock.tick(60)
 
-    def isGamerOver(self):
+    def isAlive(self):
         return len(self.balls) < 1
 
     def setRunning(self, running):
@@ -162,6 +203,9 @@ class Game:
 
     def addBall(self, ball):
         self.balls.add(ball)
+
+    def addScore(self, score):
+        self.score.add(score)
 
     def addPaddle(self, paddle):
         self.paddle.add(paddle)
@@ -183,6 +227,7 @@ def main():
 
     game.addBall(Ball())
     game.addPaddle(Paddle())
+    # game.addScore(Text("text", 32, (0,255,0), 200, 50))
     Ball.bricks = game.getBricks()
     Ball.paddle = game.getPaddle()
     # Brick.balls = game.getBalls()
